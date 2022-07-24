f4ad7624269e55b36bc49b1db2dc7ee3dd6888a4
Ioannis Sermetziadis <ioannis@codebgp.com>
2022-07-24 22:56:24 +0300

    fix: vendor mods and fix example

--- retry_test.go
+++ retry_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cenkalti/backoff"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -115,4 +116,30 @@ func (a *action) Call() error {
 		a.errors = a.errors[1:]
 	}
 	return err
+}
+
+func ExampleRetry() {
+	backoffer := &backoff.ExponentialBackOff{
+		InitialInterval:     1 * time.Millisecond,
+		RandomizationFactor: 0,
+		Multiplier:          backoff.DefaultMultiplier,
+		MaxInterval:         10 * time.Millisecond,
+		MaxElapsedTime:      10 * time.Millisecond,
+		Clock:               backoff.SystemClock,
+	}
+	backoffer.Reset()
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancelFunc()
+	recoverErrorAction := &action{errors: []error{Recoverable(errors.New("failure"))}}
+
+	Retry(ctx, recoverErrorAction.Call, backoff.NewTicker(backoffer).C, RetryRecoverablePolicy)
+
+	// Output:
+	// action called 1 time(s)
+	// action called 2 time(s)
+	// action called 3 time(s)
+	// action called 4 time(s)
+	// action called 5 time(s)
+	// action called 6 time(s)
 }
